c63cf725443777725fcff2f4d10571ba412fdb08
Patrick Cleavelin <patrick@spacegirl.nl>
2023-04-10 19:31:59 -0500

    change directory structure + nixshell

--- README.md
+++ README.md
@@ -1,33 +1,19 @@
-<img
-src="https://cdn.rawgit.com/oh-my-fish/oh-my-fish/e4f1c2e0219a17e2c748b824004c8d0b38055c16/docs/logo.svg"
-align="left" width="144px" height="144px"/>
-
-#### simple-ass-prompt
-> A theme for [Oh My Fish][omf-link].
-
-[![MIT License](https://img.shields.io/badge/license-MIT-007EC7.svg?style=flat-square)](/LICENSE)
-[![Fish Shell Version](https://img.shields.io/badge/fish-v2.2.0-007EC7.svg?style=flat-square)](http://fishshell.com)
-[![Oh My Fish Framework](https://img.shields.io/badge/Oh%20My%20Fish-Framework-007EC7.svg?style=flat-square)](https://www.github.com/oh-my-fish/oh-my-fish)
-
-<br/>
-
 ## Install
 
 ```fish
-$ omf install simple-ass-prompt
+$ fisher install pcleavelin/fish-simple-ass-prompt
 ```
+Or alternatively, just copy the `functions` directory to your fish config directory.
+
+## Fork of lfiohais's [simple-ass-prompt][original_repo]
+I've made a few changes, most notably the switch to allowing [fisher][fisher],
+but also:
+ * Displaying whether you are in a nix-shell or not.
+ * Using `rustc` for the rust version instead of `rustup`
 
 ## Features
 This is [Mathias Bynens][mths] Bash prompt ported to Fish with a few changed
-icons and added functionalities. As he put it:
-
-
-> Shell prompt based on the Solarized Dark theme.
-> Screenshot: http://i.imgur.com/EkEtphC.png
-
-> Heavily inspired by @necolas’s prompt: https://github.com/necolas/dotfiles
-
-> iTerm → Profiles → Text → use 13pt Monaco with 1.1 vertical spacing.
+icons and added functionalities.
 
 Features:
 
@@ -42,17 +28,6 @@ Features:
 - Support Rust active toolchain
 - The last command failed is displayed with `↪` in red
 
-## Screenshot
-Git Usage
-<p align="center">
-<img src="http://cl.ly/1G0Z2j3A0C1Q/Screen%20Recording%202015-12-16%20at%2010.09%20pm.gif">
-</p>
-
-[VirtualFish][vf] Usage
-<p align="center">
-<img src="http://cl.ly/image/1Y36043M2U3M/simple_ass_prompt_virtual.png">
-</p>
-
 ## Configuration
 You can change the greeting message with:
 ```fish
@@ -61,13 +36,11 @@ set -g simple_ass_prompt_greeting MyGreeting
 
 # License
 
-[MIT][mit] © [lfiolhais][author] et [al][contributors]
+[MIT][mit] © [pcleavelin][author]
 
 
 [mit]:            http://opensource.org/licenses/MIT
-[author]:         http://github.com/lfiolhais
-[contributors]:   https://github.com/lfiolhais/simple_ass_prompt/graphs/contributors
-[omf-link]:       https://www.github.com/oh-my-fish/oh-my-fish
+[author]:         http://github.com/pcleavelin
 [mths]: https://github.com/mathiasbynens/dotfiles
-[license-badge]:  https://img.shields.io/badge/license-MIT-007EC7.svg?style=flat-square
-[vf]: https://github.com/adambrenecki/virtualfish
+[original_repo]: https://github.com/lfiolhais/theme-simple-ass-prompt
+[fisher]: https://github.com/jorgebucaran/fisher
--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -4,6 +4,52 @@ set __fish_git_prompt_char_untrackedfiles '☡'
 set __fish_git_prompt_char_stashstate '↩'
 set __fish_git_prompt_char_cleanstate '✓'
 
+# Taken from OMF: https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/d427501b2c3003599bc09502e9d9535b5317c677/lib/git/git_is_repo.fish
+function git_is_repo -d "Check if directory is a repository"
+    test -d .git
+    or begin
+      set -l info (command git rev-parse --git-dir --is-bare-repository 2>/dev/null)
+      and test $info[2] = false
+    end
+end
+
+# Taken from OMF: https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/d427501b2c3003599bc09502e9d9535b5317c677/lib/git/git_ahead.fish
+function git_ahead -a ahead behind diverged none
+  not git_is_repo; and return
+
+  set -l commit_count (command git rev-list --count --left-right "@{upstream}...HEAD" 2> /dev/null)
+
+  switch "$commit_count"
+  case ""
+    # no upstream
+  case "0"\t"0"
+    test -n "$none"; and echo "$none"; or echo ""
+  case "*"\t"0"
+    test -n "$behind"; and echo "$behind"; or echo "-"
+  case "0"\t"*"
+    test -n "$ahead"; and echo "$ahead"; or echo "+"
+  case "*"
+    test -n "$diverged"; and echo "$diverged"; or echo "±"
+  end
+end
+
+# Take from OMF: https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/d427501b2c3003599bc09502e9d9535b5317c677/lib/git/git_is_stashed.fish
+function git_is_stashed -d "Check if repo has stashed contents"
+  git_is_repo; and begin
+    command git rev-parse --verify --quiet refs/stash >/dev/null
+  end
+end
+
+# Take from OMF: https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/d427501b2c3003599bc09502e9d9535b5317c677/lib/git/git_is_touched.fish
+function git_is_touched -d "Check if repo has any changes"
+  git_is_worktree; and begin
+    # The first checks for staged changes, the second for unstaged ones.
+    # We put them in this order because checking staged changes is *fast*.  
+    not command git diff-index --cached --quiet HEAD -- >/dev/null 2>&1
+    or not command git diff --no-ext-diff --quiet --exit-code >/dev/null 2>&1
+  end
+end
+
 # Display the state of the branch when inside of a git repo
 function __simple_ass_prompt_parse_git_branch_state -d "Display the state of the branch"
   git update-index --really-refresh -q 1> /dev/null
@@ -28,6 +74,16 @@ function __simple_ass_prompt_parse_git_branch_state -d "Display the state of the
 
   # Check if branch is ahead, behind or diverged of remote
   git_ahead
+end
+
+function in_nix_shell
+  test -n "$IN_NIX_SHELL"
+end
+
+# Take from OMF: https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/d427501b2c3003599bc09502e9d9535b5317c677/lib/git/git_is_worktree.fish
+function git_is_worktree -d "Check if directory is inside the worktree of a repository"
+  git_is_repo
+  and test (command git rev-parse --is-inside-git-dir) = false
 end
 
 # Display current git branch
@@ -100,10 +156,17 @@ function fish_prompt
   # Git info
   __simple_ass_prompt_git
 
+  # Nix shell
+  if in_nix_shell
+    printf (set_color blue)(echo " nix-shell")(set_color normal)
+  end
+
   # Line 2
   echo
-  if test -e "Cargo.toml"
-    printf "(rust:%s) " (set_color red)(rustup show | tail -n 3 | head -n 1 |  cut -d '-' -f 1)(set_color normal)
+  if type -q rustc
+   if test -e "Cargo.toml"
+     printf "(rust:%s) " (set_color red)(rustc --version | awk '{split($0,v,"-"); split(v[1],vn," "); split(v[2],vc," "); print vn[2] ":" vc[1];}')(set_color normal)
+   end
   end
 
   if test $VIRTUAL_ENV
